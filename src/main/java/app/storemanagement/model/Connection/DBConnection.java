9bf34c5241bf70a81a89daf2fc2158a33caa811b
TuanHHH <hotuan1162@gmail.com>
2024-05-20 13:59:09 +0700

    Thêm exe file

--- src/main/java/app/storemanagement/model/Connection/DBConnection.java
+++ src/main/java/app/storemanagement/model/Connection/DBConnection.java
@@ -7,24 +7,43 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:sqlserver://127.0.0.1;databaseName=StoreManagementDb";
-    private static final String USERNAME = System.getenv("DB_USER1");
-    private static final String PASSWORD = System.getenv("DB_PASSWORD1");
-
+//    private static final String URL = "jdbc:sqlserver://127.0.0.1;databaseName=StoreManagementDb";
+//    private static final String USERNAME = System.getenv("DB_USER1");
+//    private static final String PASSWORD = System.getenv("DB_PASSWORD1");
+//    public static Connection getConnection() {
+//        try {
+//            if(USERNAME == null || PASSWORD == null) {
+//                throw new IllegalArgumentException("Database username or password is null.");
+//            }
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            return DriverManager.getConnection(URL + ";user=" + USERNAME + ";password=" + PASSWORD + ";integratedSecurity=true;encrypt=true;trustServerCertificate=true");
+//        } catch (ClassNotFoundException | SQLException e) {
+//            System.out.println(e.getMessage());
+//            return null;
+//        }
+//    }
+//}
+//    public static Connection getConnection() {
+//        try {
+//            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//            return DriverManager.getConnection("jdbc:sqlserver://todvfpai4rtmypva0mw.mssql.somee.com:1433;databaseName=todvfpai4rtmypva0mw;user=todvfpai4rtmypv_SQLLogin_1;password=p127n22udg;encrypt=true;trustServerCertificate=true;");
+//        } catch (ClassNotFoundException | SQLException e) {
+//            System.out.println(e.getMessage());
+//            return null;
+//        }
+//    }
+//}
     public static Connection getConnection() {
         try {
-            if(USERNAME == null || PASSWORD == null) {
-                throw new IllegalArgumentException("Database username or password is null.");
-            }
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            return DriverManager.getConnection(URL + ";user=" + USERNAME + ";password=" + PASSWORD + ";integratedSecurity=true;encrypt=true;trustServerCertificate=true");
+            return DriverManager.getConnection("jdbc:sqlserver://defdcdic.mssql.somee.com:1433;databaseName=defdcdic;user=defdcdic_SQLLogin_1;"
+                    + "password=ndi9pr752l;encrypt=true;trustServerCertificate=true;");
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println(e.getMessage());
             return null;
         }
     }
 }
-
 /*public class DBConnection {
 
     private static final String DATABASE_URL = "jdbc:sqlserver://localhost";
